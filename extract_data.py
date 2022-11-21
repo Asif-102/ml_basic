5ac27bfafc6e959ac478f498d5ab3cf12680af8d
Asif-102 <asif1zero2@gmail.com>
2022-11-21 19:24:00 +0600

    find table rows

--- extract_data.py
+++ extract_data.py
@@ -4,4 +4,7 @@ from bs4 import BeautifulSoup
 text = requests.get('https://en.wikipedia.org/wiki/IPhone', auth=('user', 'pass')).text
 soup = BeautifulSoup(text, 'lxml')
 
-print(soup)
+tables = soup.find('table', class_='wikitable')
+rows = tables.find_all('tr')[1:]
+
+print(rows)
