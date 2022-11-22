ba0527efe68ccbf298082f2dd2e1086243ac0968
Asif-102 <asif1zero2@gmail.com>
2022-11-23 02:27:11 +0600

    price text split

--- extract_data.py
+++ extract_data.py
@@ -7,4 +7,13 @@ soup = BeautifulSoup(text, 'lxml')
 tables = soup.find('table', class_='wikitable')
 rows = tables.find_all('tr')[1:]
 
-print(rows)
+for row in rows:
+    data = row.find_all(['th', 'td'])
+    try:
+        version_text = data[0].a.text.split(' ')[1]
+        version = re.sub(r"\D", "", version_text)
+        print(version)
+        price_text = data[-1].text.split('/')[-1]
+        print(price_text)
+    except:
+        pass
